165acb1c566b0ba8ca9910987d3ffd53eb1f5eb1
zouzhipeng <@Zzp2739980189>
2024-03-20 09:35:46 +0800

    spring 初始化

---
+++ SpringTest/src/main/java/com/spring/test/SpringMainTest.java
@@ -0,0 +1,33 @@
+package com.spring.test;/**
+ * @Author zzp
+ * @Date 2023/7/17
+ */
+
+import com.spring.test.service.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: zzp
+ * @Date: 2023/7/17 11:21
+ */
+@Configuration
+@ComponentScan("com.spring.test")
+public class SpringMainTest {
+	public static void main(String[] args) {
+		ApplicationContext context=new AnnotationConfigApplicationContext(SpringMainTest.class);
+		UserServiceImpl bean = context.getBean(UserServiceImpl.class);
+		bean.sayHello();
+	}
+
+
+	/* public static void main(String[] args) {
+	 ApplicationContext context=new AnnotationConfigApplicationContext(SpringMainTest.class);
+	 EnhanceClass bean = context.getBean(EnhanceClass.class);
+	 bean.enhance();
+	 }*/
+
+}
